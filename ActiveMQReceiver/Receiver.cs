ea069c5c0178b2f22c41453d2a51d0a671087bcb
Kęstutis Keršis <kkersis@alnasoft.lt>
2021-08-24 15:35:02 +0300

    Add project files.

---
+++ ActiveMQReceiver/Receiver.cs
@@ -0,0 +1,53 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActiveMQReceiver
+{
+    public partial class Receiver : Form
+    {
+        private const string URL = "tcp://localhost:61616";
+        IConnection conn;
+        public Receiver()
+        {
+            InitializeComponent();
+            this.Text = "Receiver";
+        }
+
+        private void Message_Listener(IMessage message)
+        {
+            ITextMessage textMessage = (ITextMessage)message;
+            string receivedText = (string)textMessage.Text;
+            MessageBox.Show(receivedText);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (conn != null && conn.IsStarted)
+            {
+                conn.Close();
+                button1.Text = "Start Receiver";
+            }
+            else
+            {
+                IConnectionFactory connFactory = new ConnectionFactory(URL);
+                conn = connFactory.CreateConnection();
+                ISession session = conn.CreateSession();
+                conn.Start();
+                IDestination dest = SessionUtil.GetDestination(session, "pavyzdine.eile");
+                IMessageConsumer receiver = session.CreateConsumer(dest);
+                receiver.Listener += new MessageListener(Message_Listener);
+                button1.Text = "Stop Receiver";
+            }
+        }
+    }
+}
---
+++ ActiveMQSender/Sender.cs
@@ -0,0 +1,43 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActiveMQSender
+{
+    public partial class Sender : Form
+    {
+        private const string URL = "tcp://localhost:61616";
+
+        public Sender()
+        {
+            var receiverForm = new ActiveMQReceiver.Receiver();
+            receiverForm.Show(); 
+            InitializeComponent();
+            this.Text = "Sender";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IConnectionFactory connFactory = new ConnectionFactory(URL);
+            using IConnection conn = connFactory.CreateConnection();
+            using ISession session = conn.CreateSession();
+            //IObjectMessage
+            ITextMessage textMessage = session.CreateTextMessage(textBox1.Text);
+            textMessage.NMSMessageId = "1";
+            conn.Start();
+            IDestination dest = SessionUtil.GetDestination(session, "pavyzdine.eile");
+            IMessageProducer messageProducer = session.CreateProducer(dest);
+
+            messageProducer.Send(textMessage);
+        }
+    }
+}
